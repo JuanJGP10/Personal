3a9411d4399526a9980a4a406996bf9d48b429cc
JuanJGP8 <jjgphillips1@gmail.com>
2025-05-08 14:00:26 +0200

    adad

--- task-manager/src/main/java/project/task_manager/model/Task.java
+++ task-manager/src/main/java/project/task_manager/model/Task.java
@@ -1,14 +1,25 @@
 package project.task_manager.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tasks")
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String titulo;
+
     private boolean completada;
+
+    public Task() {
+    }
 
     public Task(Long id, String titulo, boolean completada) {
         this.id = id;
